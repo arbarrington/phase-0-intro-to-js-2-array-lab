341e0a50e703093f5bebbbb6221567d73fbf8c5d
Austin Barrington <arbarrington12@gmail.com>
2022-08-09 17:00:35 -0600

    commit tree a0933c82d1386dc3af3848f3b489474cd5d0eef9

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.splice(2);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.splice(0,1);
+}
+
+function appendCat(name) {
+    return [...cats,name];
+}
+
+function prependCat(name) {
+    return [name,...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0,2);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
